bef26df77fc40a03513a11c9b22a127663280056
Kaio Fonseca <mkaiofonseca@gmail.com>
2023-01-27 10:56:47 -0300

    bug resolvido

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Main from "./Components/Main";
+import Marvel from "./Components/Marvel";
 import './Components/styles.css'
 import {Routes, Route} from 'react-router-dom'
 
@@ -7,6 +8,7 @@ function App() {
     <>
         <Routes>
           <Route path="/" element={<Main/>}/>
+          <Route path="/:id" element={<Marvel/>}/>
         </Routes>
     </>
   );
--- src/Components/Marvel.js
+++ src/Components/Marvel.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import { useState } from 'react'
+import axios from 'axios';
+import { useParams } from 'react-router-dom'
 
 const Marvel = () => {
+  const {id} = useParams()
+  const [item, setItem] = useState()
+  const fetch = async() => {
+    const res = await axios.get(`http://gateway.marvel.com/v1/public/characters/${id}?ts=1&apikey=5b5fabd7ee01770e82fa88b5a8e17546&hash=59c00475557a00fc0f17be2863fbd27e`)
+    setItem(res.data.data.results[0])
+  }
+  fetch()
   return (
     <>
-    <p>
-        lorem10 lorem10 lorem10 lorem10lorem10lorem10lorem10lorem10lorem10lorem10  lorem10 lorem10 lorem10
-    </p>
+    {
+      (!item) ? "" : (
+        <div className='box-content'>
+          <div className='right-box'>
+            <img src={`${item.thumbnail.path}.${item.thumbnail.extension}`} alt='' />
+          </div>
+          <div className='left-box'>
+            <h1>{item.name}</h1>
+            <h4>{item.description}</h4>
+          </div>
+        </div>
+      )
+    }
     </>
   )
 }
